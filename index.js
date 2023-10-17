a6f8f616144155dc73644494e3480a6365f7d7be
Angekarara <mbabange2020@gmail.com>
2023-10-17 23:16:50 +0200

    all files

---
+++ index.js
@@ -0,0 +1,11 @@
+const navBar = (
+  <nav>
+    <h1>Burger Planet</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>Contact</li>
+      <li>About</li>
+    </ul>
+  </nav>
+);
+ReactDOM.render(navBar, document.getElementById("root"));
