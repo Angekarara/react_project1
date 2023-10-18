6ba0e0da18686aa13b153c40b108d7cb1ab09547
Angekarara <mbabange2020@gmail.com>
2023-10-18 23:30:43 +0200

    new changes

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const page = (
   <div>
     <h1>My awesome website in React</h1>
     <h3>Reason i love react</h3>
-    <ul>
-      <li>Home</li>
-      <li>Prices</li>
-      <li>Contact</li>
-      <li>About</li>
-    </ul>
+    <ol>
+      <li>It's composable</li>
+      <li>It's declarative</li>
+      <li>It's a hireable skill</li>
+      <li>It's actively maintained by skilled people</li>
+    </ol>
   </div>
 );
 ReactDOM.createRoot(document.getElementById("root")).render(page);
